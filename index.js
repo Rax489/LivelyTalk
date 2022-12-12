2ab72b3c580127fc5e7c9cca299904f8988f4e36
Stoyanowww <pstoyanov04@gmail.com>
2022-12-12 19:55:23 +0200

    Created the structure and connected the DB

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require("./routes/db-config");
+const app = express();
+const cookie = require("cookie-parser");
+const PORT = process.env.PORT;
+
+app.use("/js", express.static(__dirname + "./public/js"))
+app.use("/css", express.static(__dirname + "./public/css"))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(cookie());
+app.use(express.json());
+db.connect((err) => {
+    if(err) throw err;
+    console.log("Data base was connencted succefully!");
+})
+
+app.listen(PORT);
